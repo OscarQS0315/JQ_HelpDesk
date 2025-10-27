9e774c52e476adc2dc8a2a8bc3d413f43c213d15
Oscar Quirós Sequeira <osquirosse@est.utn.ac.cr>
2025-10-27 00:13:00 -0600

    image config

--- server/source/server.ts
+++ server/source/server.ts
@@ -33,7 +33,7 @@ app.use(AppRoutes.routes)
 app.use(ErrorMiddleware.handleError);
 
 //Acceso a las imágenes
-
+app.use("/images", express.static(path.join(path.resolve(), "assets/images")));
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
